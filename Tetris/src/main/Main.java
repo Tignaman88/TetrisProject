9ad77321667cf3edacbdd963c829ceefe9ba3bcf
Tignaman88 <alessioscarpello2809@gmail.com>
2025-04-01 18:50:36 +0100

    Window Created

--- Tetris/src/main/Main.java
+++ Tetris/src/main/Main.java
@@ -1,4 +1,16 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("Simple Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
 }
